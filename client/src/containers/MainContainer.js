2c4db6b8d34bd04572727fd71535318a45e6526e
Kevin McDermott <kmcdermott31@gmail.com>
2020-01-15 22:10:33 +0000

    Working average between LHR and DXB

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Request from '../helpers/request';
 import { stat } from 'fs';
+import moment from 'moment';
 
 class MainContainer extends Component {
     constructor(props) {
@@ -11,6 +12,7 @@ class MainContainer extends Component {
         }
         this.findAllSwedishArrivals = this.findAllSwedishArrivals.bind(this);
         this.findAllMorningFlights = this.findAllMorningFlights.bind(this);
+        this.calculateAverageDubaiFlightTime = this.calculateAverageDubaiFlightTime.bind(this);
     }
 
     componentDidMount() {
@@ -53,12 +55,51 @@ class MainContainer extends Component {
         return (counter / this.state.flights.length).toFixed(4);
     }
 
+    // Calculate the 10 most popular destinations
+    
+    // Calculate the average time between LHR and DXB
+    calculateAverageDubaiFlightTime() {
+        // Create Counter and DurationTotal
+        let counter = 0;
+        let durationTotal = 0;
+        // Loop through flight data
+        this.state.flights.forEach(flight => {
+            // If the flights depair = LHR and destair = DXB
+            if (flight.depair == "LHR" && flight.destair == "DXB"){
+                // Add 2 to counter for return flight
+                counter += 2;
+                // Subtract the flights arrival time from the departure Time and add to the DurationTotal for Inbound flights.
+                const inboundDepartureTime = flight.indepartdate + ' ' + flight.indeparttime;
+                const inboundArrivalTime = flight.inarrivaldate + ' ' + flight.inarrivaltime;
+                const inboundDiff = moment.duration(moment(inboundArrivalTime).diff(moment(inboundDepartureTime)));
+                // Add 4 hours on for time difference
+                const inboundMinutes = parseInt(inboundDiff.asMinutes()) + 240;
+                durationTotal += inboundMinutes;
+                // Subtract the flights arrival time from the departure time and add to DurationTotal for outbound flights.
+                const outboundDepartureTime = flight.outdepartdate + ' ' + flight.outdeparttime;
+                const outboundArrivalTime = flight.outarrivaldate + ' ' + flight.outarrivaltime;
+                const outboundDiff = moment.duration(moment(outboundArrivalTime).diff(moment(outboundDepartureTime)));
+                // Subtract 4 hours for the time difference
+                const outboundMinutes = parseInt(outboundDiff.asMinutes()) - 240;
+                durationTotal += outboundMinutes;
+            }
+        })
+        // Calculate the average by adding all the flight times and dividing by the counter
+        // Rounded the average to a whole number to make displaying it tidy.
+        const averageFlightTime = (durationTotal / counter).toFixed(0);
+        const averageHours = Math.floor(averageFlightTime / 60);
+        const averageMinutes = averageFlightTime % 60;
+        // Return a simple string to display the findings
+        return `The average flight time between LHR and DXB is ${averageHours} hours and ${averageMinutes} minutes.`
+    }
+
     render() { 
         return ( 
             <div>
                 <div>
                     <p>There is {this.findAllMorningFlights()} morning flights.</p>
                     <p>Only {this.findAllSwedishArrivals()}% of flights on this data fly into Sweden.</p>
+                    <p>{this.calculateAverageDubaiFlightTime()}</p>
                 </div>
             </div>
          );
