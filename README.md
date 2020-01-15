40c6d02b977725273663d3a310bd75e792576184
Kevin McDermott <kmcdermott31@gmail.com>
2020-01-15 20:11:27 +0000

    Have working percentage of swedish destair

---
+++ README.md
@@ -0,0 +1,16 @@
+## Server
+
+Navigate to the server directory.
+
+`npm install` will install required dependancies.
+
+`npm run server:dev` will start up the backend which acts as a local api running on [http://localhost:3000](http://localhost:3000).
+
+## Client
+
+Navigate to the client directory.
+
+`npm install` will install the required dependancies.
+
+`npm start` will try to start the front end at [http://localhost:3000](http://localhost:3000) but this is being used to run the back-end so respond `y` to the prompt about running on another port which will cause it to most likely launch on [http://localhost:3001](htpp://localhost:3001)
+
--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -6,28 +6,45 @@ class MainContainer extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            flights: []
-         }
+            flights: [],
+            swedishArrivals: 0
+        }
+        this.findAllSwedishArrivals = this.findAllSwedishArrivals.bind(this);
     }
 
     componentDidMount() {
+        // Fetch the data from the API hosted in our server side folder
         fetch('http://localhost:3000/')
         .then(results => {
             return results.json()
         })
+        // Update the state so all of our flight data is updated in the app
         .then(data => {
-            console.log(data[0]);
             this.setState({flights: data})
         })
+    }
+
+    // Calculate how many flights arrive in airports with swedish IATA codes (Seems to only be ARN and GOT)
+    findAllSwedishArrivals() {
+        let counter = 0;
+        // Loop through our Array of flights from the state
+        this.state.flights.forEach(flight => {
+            // If the flights Destair is includes the swedish IATA codes then add 1 to counter.
+            if (flight.destair.includes("ARN") || flight.destair.includes("GOT")) {
+                counter ++;
+            }
+            
+        })
+        // Return the number of flights a percentage of all flights in data
+        return (counter / this.state.flights.length).toFixed(4);
     }
 
     render() { 
         return ( 
             <div>
                 <div>
-                {this.state.flights.map(flight => <p><b>Carrier:</b> {flight.carrier}<br/><b>Departure Airport:</b> {flight.depair}<br/><b>Destination Airport:</b> {flight.destair}</p>)}
+                    <p>Only {this.findAllSwedishArrivals()}% of flights on this data fly into Sweden.</p>
                 </div>
-                <p></p>
             </div>
          );
     }
